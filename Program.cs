6ab7b678fa64ec5a778ef49585b0d8f2b2fa20e6
Dmitriy <x5retail20@gmail.com>
2022-08-15 11:57:55 +0300

    num1 * num1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Число помноженное на себя");
+Console.WriteLine("Введи число и оно умножиться на себя");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int result = num1 * num1;
+Console.WriteLine(result);
